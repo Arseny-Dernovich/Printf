a6d4b54c8babe3eb9fcf4f66e20a0ed32dc0aa49
Arseny-Dernovich <arseni.dernovich@bk.ru>
2025-03-18 18:09:31 +0300

    Add files via upload
    %c c vaargs

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    my_printf("Hello %corld \n", 'W');
+    my_printf("Hello %c%c%c%c%c%c %c %c %c  \n", 'W' , 'o' , 'l' , 'd' , 'r' , '!'  , 's' , 'x');
 
     
 }                                           
