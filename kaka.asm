db92a39909a1db3b629ff2db5c2a91c34e493c42
Arseny-Dernovich <arseni.dernovich@bk.ru>
2025-03-18 18:09:02 +0300

    %с c vaargs

--- kaka.asm
+++ kaka.asm
@@ -1,8 +1,9 @@
+%define arg(id) [rbp + 8 + 8 * id]
 section .bss
     buffer resb 256  
     
 section .text
-    global my_printf, _start
+    global my_printf, _start , cdecl_print
     extern main  ; main определён в другом файле (например, на C)
 
 _start:
@@ -11,18 +12,29 @@ _start:
     mov rdi, 0    ; код возврата 0
     mov rax, 60   ; syscall: exit
     syscall       ; завершаем программу
-    
+
 my_printf:
-    push    rbp 
-    mov     rbp , rsp                                                          
-    push    rbx 
-    ; push    rdi          
-    ; push    rsi          
+
+    ; Пушим все необходимые регистры с аргументами
+    push    r9
+    push    r8
+    push    rcx
+    push    rdx
+    push    rsi
+    push    rdi        
     
-    mov     rbx, buffer  
+cdecl_print:
+
+    push    rbp                 ; Сохраняем старое значение rbp
+    mov     rbp, rsp            ; Устанавливаем новый rbp
+
+    mov     r8 , 1                 ; счётчик аругментов
+
+    mov     rbx, buffer     
     mov     rdx, 0       ; Счётчик длины строки
 
 .loop:
+
     mov     al, byte [rdi]  ; Загружаем текущий символ                      
     test    al, al          ; Проверяем конец строки
     jz      .flush          ; Если 0 (конец), печатаем буфер
@@ -35,9 +47,9 @@ my_printf:
     cmp     al, 'c'         ; Проверяем "%c"
     jne     .store_char
 
-    mov     al, sil         ; Загружаем переданный символ
-    inc     rsi             ; Смещаемся на следующий аргумент
-    jmp     .store_char
+    cmp     r8 , 6
+    je      .skip_addr_ret
+    jmp     .parse_args
 
 .store_char:
     mov     [rbx], al       ; Кладём символ в буфер
@@ -45,6 +57,19 @@ my_printf:
     inc     rdi             ; Переходим к следующему символу
     inc     rdx             ; Увеличиваем счётчик длины
     jmp     .loop
+
+
+.skip_addr_ret:
+
+    inc r8
+    jmp .parse_args
+
+.parse_args:
+
+
+    mov     al, arg(r8)                       ; Загружаем переданный символ
+    inc     r8
+    jmp     .store_char
 
 .flush:
     mov     rax, 0x01       ; syscall write
@@ -54,8 +79,15 @@ my_printf:
 
             ; pop     rsi
             ; pop     rdi
-    pop     rbx
-    pop     rbp             
+            
+    pop     rbp  
+    pop     rdi
+    pop     rsi
+    pop     rdx
+    pop     rcx
+    pop     r8
+    pop     r9
+
     ret
         
             
